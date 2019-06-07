131c4dd1e5cf4aaa291b58fc98ae9eda78592dbc
tb59 <thomas.bourdeaudhuy@gmail.com>
2019-06-07 16:06:44 +0200

    V2 (fichier texte)

--- data.php
+++ data.php
@@ -10,10 +10,18 @@ if (isset($_GET["debutNom"]))
 	foreach ($tabLignes as $ligne)
 	{
 		// EXO1 : effectuer une recherche sur nom ou prénom 
-		if (preg_match("/^.*:(" . $cherche . ".*)$/i",$ligne,$tabResultats))
+		if (preg_match("/^(.*):(" . $cherche . ".*)$/i",$ligne,$tabResultats))
 		{
+
+// Si l'utilisateur saisit debutNom=Th
+// L'expression régulière vaut : 
+//		/^.*:(Th.*)$/i
+// On cherche toutes les lignes dont la deuxième partie commence par Th, peu importe la casse
+
+// On renvoie en respectant le format attendu
+
 			// EXO2 afficher nom ET prénom 
-			echo $tabResultats[1]; 
+			echo "<li> $tabResultats[1] $tabResultats[2] </li>"; 
 		}
 	}
 
